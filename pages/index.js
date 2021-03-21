86812cf36ff83f5e0763be3fa556ab77758ab6ec
grind-t <grindt.gd@gmail.com>
2021-03-21 13:08:21 +0300

    add header and shopping cart

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,28 @@
 import Head from 'next/head'
-import styles from 'styles/Page.module.css'
+import Link from 'next/link'
+import pageStyles from 'styles/page.module.css'
+import styles from './index.module.css'
 
 export default function Main() {
   return (
-    <div className={styles.container}>
+    <div className={pageStyles.container}>
       <Head>
         <title>Online store</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <header className={pageStyles.header}>
+        <Link href="/cart">
+          <a className={styles.cart}>
+            <span className={styles.cartTotalPrice}>520 ₽</span>
+            <img
+              src="/shopping-cart.svg"
+              alt="Корзина"
+              className={styles.cartIcon}
+            />
+            <span className={styles.cartItemsCount}>3</span>
+          </a>
+        </Link>
+      </header>
     </div>
   )
 }
