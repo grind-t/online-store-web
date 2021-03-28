0952e8de395a8293371066f09c7cce053bd3ceb2
grind-t <grindt.gd@gmail.com>
2021-03-28 22:37:39 +0300

    improve accesebility

--- components/cart-link.tsx
+++ components/cart-link.tsx
@@ -6,8 +6,11 @@ export default function CartLink() {
   return (
     <Link href="/cart">
       <a className={styles.cart}>
+        <span className="sr-only">Корзина</span>
+        <span className="sr-only">Сумма</span>
         <span className={styles.totalPrice}>520 ₽</span>
         <CartIcon stroke="white" strokeWidth="1.8" className={styles.icon} />
+        <span className="sr-only">Количество товаров</span>
         <span className={styles.itemsCount}>3</span>
       </a>
     </Link>
