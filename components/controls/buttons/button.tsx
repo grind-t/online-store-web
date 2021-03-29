3efd3c1c35602acb9be076f33a8888bc08564fd1
grind-t <grindt.gd@gmail.com>
2021-03-29 22:20:59 +0300

    create base button

---
+++ components/controls/buttons/button.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const Button = styled.button`
+  padding: 1.21rem 2.85rem;
+  margin: 0;
+  background: none;
+  border: none;
+  border-radius: 2.14rem;
+  font-size: 1.14rem;
+  line-height: 1.57rem;
+`
+
+export default Button
