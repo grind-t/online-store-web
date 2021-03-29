6238ddc1df6d6e557857acbfc22f7ad83ea6ec71
grind-t <grindt.gd@gmail.com>
2021-03-29 22:42:14 +0300

    create category button

---
+++ components/controls/buttons/category-button.tsx
@@ -0,0 +1,16 @@
+import Button from 'components/controls/buttons/button'
+import styled from 'styled-components'
+
+const CategoryButton = styled(Button)`
+  min-width: 5.93rem;
+  padding: 0.8rem 1.07rem 0.8rem 1.43rem;
+  margin-right: 0.5rem;
+  border-radius: 7.14rem 4.29rem 4.29rem 2.14rem;
+  background-color: #282828;
+  font-size: 1.29rem;
+  line-height: 1.39;
+  letter-spacing: 0.015em;
+  color: white;
+`
+
+export default CategoryButton
