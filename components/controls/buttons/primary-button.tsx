83a332030c8f69cf601abc84506665a9e2025c20
grind-t <grindt.gd@gmail.com>
2021-03-29 22:21:17 +0300

    create primary button

---
+++ components/controls/buttons/primary-button.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+import Button from 'components/controls/buttons/button'
+
+const PrimaryButton = styled(Button)`
+  background: #fe5f1e;
+  color: white;
+  font-weight: bold;
+`
+
+export default PrimaryButton
