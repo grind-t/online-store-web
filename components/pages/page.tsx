46a6bbc94082b43670841a34bf7ce5afdff5b730
grind-t <grindt.gd@gmail.com>
2021-03-29 21:51:22 +0300

    refactor page to styled component

---
+++ components/pages/page.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+const Header = styled.header`
+  display: flex;
+  align-items: flex-end;
+  height: 9.29rem;
+  border-bottom: 1px solid #f7f7f7;
+`
+
+const Page = styled.div`
+  margin: 10px 10px 0 10px;
+  min-height: calc(100vh - 20px);
+  background-color: #ffffff;
+  border-radius: 10px;
+`
+export { Page as default, Header }
