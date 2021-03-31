957f589f3256ad7d733b27f73bdbe6c4c9682b2d
grind-t <grindt.gd@gmail.com>
2021-03-31 19:27:37 +0300

    refactor category button

--- components/controls/buttons/category-button.tsx
+++ components/controls/buttons/category-button.tsx
@@ -1,15 +1,14 @@
 import Button from 'components/controls/buttons/button'
 import styled from 'styled-components'
 
-const CategoryButton = styled(Button)`
+const CategoryButton = styled(Button).attrs({
+  font: '18',
+})`
   min-width: 5.93rem;
   padding: 0.8rem 1.07rem 0.8rem 1.43rem;
   margin-right: 0.5rem;
   border-radius: 7.14rem 4.29rem 4.29rem 2.14rem;
   background-color: #282828;
-  font-size: 1.29rem;
-  line-height: 1.39;
-  letter-spacing: 0.015em;
   color: white;
 `
 
