5606dea536bc5de9370300268a31c43c6fe0f63d
grind-t <grindt.gd@gmail.com>
2021-03-31 19:27:54 +0300

    refactor primary button

--- components/controls/buttons/primary-button.tsx
+++ components/controls/buttons/primary-button.tsx
@@ -4,7 +4,6 @@ import Button from 'components/controls/buttons/button'
 const PrimaryButton = styled(Button)`
   background: #fe5f1e;
   color: white;
-  font-weight: bold;
 `
 
 export default PrimaryButton
