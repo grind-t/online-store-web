4581b5d0d1d437247d98046692af752b52ea5d7d
grind-t <grindt.gd@gmail.com>
2021-03-31 18:41:23 +0300

    refactor Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,83 @@
 import Head from 'next/head'
-import Link from 'next/link'
-import SortBy from 'components/sort-by'
-import pageStyles from 'styles/page.module.css'
-import styles from './index.module.css'
+import Page, { Header } from 'components/pages/page'
+import CartLink from 'components/links/cart-link'
+import CategoryButton from 'components/controls/buttons/category-button'
+import SortBy from 'components/controls/lists/sort-by'
+import ProductTile from 'components/tiles/product-tile'
+import { Heading } from 'components/typography'
+import styled from 'styled-components'
 
-export default function Main() {
+const Main = styled.main`
+  margin: 4.29rem 5.86rem 0 5.86rem;
+`
+
+const ProductsBar = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+const Categories = styled.ul`
+  padding: 0;
+  margin: 0;
+  list-style: none;
+`
+
+const Category = styled.li`
+  display: inline-block;
+  padding: 0;
+  margin: 0;
+`
+
+const ProductsHeading = styled(Heading).attrs({
+  as: 'h2',
+  font: '32',
+})`
+  margin-top: 0.5rem;
+`
+
+const Products = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 2.57rem;
+  margin-top: 6rem;
+`
+
+const Home = () => {
   return (
-    <div className={pageStyles.container}>
+    <Page>
       <Head>
-        <title>Online store</title>
+        <title>TODO</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header className={pageStyles.header}>
-        <Link href="/cart">
-          <a className={styles.cart}>
-            <span className={styles.cartTotalPrice}>520 ₽</span>
-            <img
-              src="/shopping-cart.svg"
-              alt="Корзина"
-              className={styles.cartIcon}
-            />
-            <span className={styles.cartItemsCount}>3</span>
-          </a>
-        </Link>
-      </header>
-      <main className={styles.main}>
-        <div className={styles.productsBar}>
-          <button className={styles.productsBarButton}>Все</button>
-          <button className={styles.productsBarButton}>Playstation Plus</button>
-          <SortBy options={['популярности', 'цене', 'алфавиту']} />
-        </div>
-        <h1 className={styles.heading}>Все товары</h1>
-      </main>
-    </div>
+      <Header>
+        <CartLink />
+      </Header>
+      <Main>
+        <h1 className="sr-only">Товары</h1>
+        <section>
+          <h2 className="sr-only">Показать товары</h2>
+          <ProductsBar>
+            <Categories>
+              <Category>
+                <CategoryButton>Все</CategoryButton>
+              </Category>
+              <Category>
+                <CategoryButton>Playstation Plus</CategoryButton>
+              </Category>
+            </Categories>
+            <SortBy options={['популярности', 'цене', 'алфавиту']} />
+          </ProductsBar>
+        </section>
+        <section>
+          <ProductsHeading>Все товары</ProductsHeading>
+          <Products>
+            <ProductTile />
+            <ProductTile />
+          </Products>
+        </section>
+      </Main>
+    </Page>
   )
 }
+
+export default Home
