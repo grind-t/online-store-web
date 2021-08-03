f1425722db76936175e120f223c258f5d20091e2
grind-t <grindt.gd@gmail.com>
2021-08-03 13:44:30 +0300

    fix burger-menu-icon

--- components/atoms/icons/burger-menu-icon.tsx
+++ components/atoms/icons/burger-menu-icon.tsx
@@ -3,11 +3,16 @@ interface BurgerMenuIconProps {
 }
 
 const BurgerMenuIcon = ({ className }: BurgerMenuIconProps) => (
-  <svg viewBox="0 0 22 14" fill="none" className={className} aria-hidden>
-    <rect x="3" y="6" width="19" height="2" rx="1" fill="black" />
-    <rect x="7.33334" y="12" width="14.6667" height="2" rx="1" fill="black" />
-    <rect width="22" height="2" rx="1" fill="black" />
-    <rect width="22" height="2" rx="1" stroke="black" />
+  <svg
+    width="22"
+    viewBox="0 0 22 14"
+    fill="black"
+    className={className}
+    aria-hidden
+  >
+    <rect x="3" y="6" width="19" height="2" rx="1" />
+    <rect x="7.33334" y="12" width="14.6667" height="2" rx="1" />
+    <rect width="22" height="2" rx="1" />
   </svg>
 );
 
