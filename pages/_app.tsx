40b6635d781fb64dbc383b9ed54c1e1ee79512ee
grind-t <grindt.gd@gmail.com>
2021-08-04 17:48:37 +0300

    install firebase

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import { AppProps } from 'next/app';
 import { createGlobalStyle } from 'styled-components';
 import { UIDReset } from 'react-uid';
 import { miniReset, nunitoFont } from 'styles/mixins';
+import { FirebaseAppProvider } from 'reactfire';
 
 const GlobalStyle = createGlobalStyle`
   ${nunitoFont()}
@@ -18,12 +19,24 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+};
+
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <GlobalStyle />
       <UIDReset>
-        <Component {...pageProps} />
+        <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+          <Component {...pageProps} />
+        </FirebaseAppProvider>
       </UIDReset>
     </>
   );
