635b47d7394ffd123a1847df009454900c3a361a
grind-t <grindt.gd@gmail.com>
2021-08-26 01:16:00 +0300

    add cart logic to product-card

--- components/molecules/product-card.tsx
+++ components/molecules/product-card.tsx
@@ -1,10 +1,18 @@
-import styled from 'styled-components';
-import Image from 'next/image';
-import { ChangeEvent, useState } from 'react';
-import { em } from 'styles/mixins';
-import CustomInput from 'components/atoms/utils/custom-input';
+import { lineItemChanged, selectLineItem } from 'app/redux/cart-slice';
+import { useAppSelector } from 'app/redux/hooks';
 import AddToCartButton from 'components/atoms/buttons/add-to-cart-button';
-import { Product, getVariant, selectInitialOptions } from 'lib/product';
+import CustomInput from 'components/atoms/utils/custom-input';
+import {
+  Product,
+  getVariant,
+  selectInitialOptions,
+  ProductVariants,
+} from 'lib/product';
+import Image from 'next/image';
+import { ChangeEvent, useMemo, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import styled from 'styled-components';
+import { em } from 'styles/mixins';
 
 //#region styled
 // https://github.com/vercel/next.js/discussions/18312
@@ -54,19 +62,36 @@ const PurchaseContainer = styled.div`
 
 interface ProductCardProps {
   product: Product;
+  variants: ProductVariants;
 }
 
-const ProductCard = ({ product }: ProductCardProps) => {
+const ProductCard = ({ product, variants }: ProductCardProps) => {
   const [selectedOptions, setSelectedOptions] = useState(() =>
     selectInitialOptions(product.options)
   );
-  const variant = getVariant(product.variants, selectedOptions);
+  const variant = useMemo(
+    () => getVariant(Object.values(variants), selectedOptions),
+    [variants, selectedOptions]
+  );
+  const lineItemId = `${product.id}_${variant.id}`;
+  const lineItem = useAppSelector(selectLineItem(lineItemId));
+  const quantity = lineItem ? lineItem.quantity : 0;
   const image = variant.image || product.image;
   const price = variant.price || product.price;
+  const dispatch = useDispatch();
 
   const handleOptionChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setSelectedOptions((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleAddToCart = () => {
+    const lineItem = {
+      productId: product.id,
+      variantId: variant.id,
+      quantity: quantity + 1,
+    };
+    dispatch(lineItemChanged(lineItemId, lineItem));
   };
 
   return (
@@ -101,7 +126,7 @@ const ProductCard = ({ product }: ProductCardProps) => {
       )}
       <PurchaseContainer>
         <Price>от {price.value} ₽</Price>
-        <AddToCartButton />
+        <AddToCartButton count={quantity} onClick={handleAddToCart} />
       </PurchaseContainer>
     </>
   );
