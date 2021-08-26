ed5ececbc3b4cf20d44e4ecfa9aaf9ee1cf71d01
grind-t <grindt.gd@gmail.com>
2021-08-26 23:20:41 +0300

    add go-back-button component

---
+++ components/atoms/buttons/go-back-button.tsx
@@ -0,0 +1,26 @@
+import { useRouter } from 'next/router';
+import styled from 'styled-components';
+
+//#region styled
+const Button = styled.button`
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  width: 210px;
+  min-height: 55px;
+  background: white;
+  border: 1px solid #d3d3d3;
+  border-radius: 30px;
+  color: #cacaca;
+  font-size: 1rem;
+  text-align: center;
+  cursor: pointer;
+`;
+//#endregion
+
+const GoBackButton = () => {
+  const router = useRouter();
+  return <Button onClick={() => router.back()}>Вернуться назад</Button>;
+};
+
+export default GoBackButton;
