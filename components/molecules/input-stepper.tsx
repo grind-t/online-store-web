22d670f77746f5ac9a00842f72a04cb9729a541c
grind-t <grindt.gd@gmail.com>
2021-08-27 15:39:12 +0300

    add input-stepper component

--- components/molecules/input-stepper.tsx
+++ components/molecules/input-stepper.tsx
@@ -1,63 +1,78 @@
-import MinusIcon from 'components/atoms/icons/minus-icon';
-import PlusIcon from 'components/atoms/icons/plus-icon';
-import { useUID } from 'react-uid';
-import styled, { css } from 'styled-components';
+import StandaloneMinusIcon from 'components/atoms/icons/minus-icon';
+import StandalonePlusIcon from 'components/atoms/icons/plus-icon';
+import VisuallyHidden from 'components/atoms/utils/visually-hidden';
+import styled from 'styled-components';
+import { up } from 'styles/mixins';
+import { breakpoints } from 'styles/varibles';
 
-//#region
-const Container = styled.div`
-  display: inline-flex;
-  justify-content: center;
-  align-items: center;
+//#region styled
+const xsFontSize = 14;
+const xlFontSize = 22;
+
+const MinusIcon = styled(StandaloneMinusIcon)`
+  width: 0.43em;
+  height: 0.09em;
+`;
+
+const PlusIcon = styled(StandalonePlusIcon)`
+  width: 0.43em;
+  height: 0.43em;
 `;
 
 const Button = styled.button`
-  padding: 0.657rem;
-  border: 0.143rem solid #fe5f1e;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  width: 1.45em;
+  height: 1.45em;
+  background: white;
+  border: 1.5px solid #fe5f1e;
   border-radius: 50%;
-  background: none;
-  line-height: 0;
 `;
 
-const Output = styled.output`
-  margin: 0 0.571rem;
-  font-size: 1.571rem;
+const Value = styled.span`
+  margin: 0 0.375em;
   font-weight: bold;
   letter-spacing: 0.01em;
 `;
 
-const icon = css`
-  width: 0.686rem;
-  height: 0.686rem;
-  fill: #fe5f1e;
-`;
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: ${xsFontSize}px;
 
-const DecreaseIcon = styled(MinusIcon)`
-  ${icon}
-`;
+  @media ${up(breakpoints.md)} {
+    font-size: ${(xlFontSize + xsFontSize) / 2}px;
+  }
 
-const IncreaseIcon = styled(PlusIcon)`
-  ${icon}
+  @media ${up(breakpoints.xl)} {
+    font-size: ${xlFontSize}px;
+  }
 `;
 //#endregion
 
 interface InputStepperProps {
   value?: number | string;
-  onDecrease?: () => void;
-  onIncrease?: () => void;
+  onDecrement?: () => void;
+  onIncrement?: () => void;
 }
 
-const InputStepper = ({ value, onDecrease, onIncrease }: InputStepperProps) => {
-  const decreaseId = useUID();
-  const increaseId = useUID();
-
+const InputStepper = ({
+  value,
+  onDecrement,
+  onIncrement,
+}: InputStepperProps) => {
   return (
     <Container>
-      <Button id={decreaseId} onClick={onDecrease}>
-        <DecreaseIcon />
+      <Button onClick={onDecrement}>
+        <VisuallyHidden>Меньше</VisuallyHidden>
+        <MinusIcon />
       </Button>
-      <Output htmlFor={`${decreaseId} ${increaseId}`}>{value}</Output>
-      <Button id={increaseId} onClick={onIncrease}>
-        <IncreaseIcon />
+      <Value>{value}</Value>
+      <Button onClick={onIncrement}>
+        <VisuallyHidden>Больше</VisuallyHidden>
+        <PlusIcon />
       </Button>
     </Container>
   );
