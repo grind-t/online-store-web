7b8cc8374cc4e56c7c82183a0bef7ab4ccb530f0
grind-t <grindt.gd@gmail.com>
2021-09-01 20:39:12 +0300

    add HeadingLevel type

--- lib/utils.ts
+++ lib/utils.ts
@@ -3,3 +3,5 @@ export type EntityId = string;
 export type Entity<T> = [EntityId, T];
 
 export type Entities<T> = Record<EntityId, T>;
+
+export type HeadingLevel = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
