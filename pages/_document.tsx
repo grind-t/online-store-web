7933e8f0337b93ba8af39e77511fdebc0c88f314
grind-t <grindt.gd@gmail.com>
2021-09-16 23:14:51 +0300

    temporary fix due to nextjs bug

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable-next-line */
 import Document, { DocumentContext } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
@@ -9,6 +10,7 @@ export default class MyDocument extends Document {
     try {
       ctx.renderPage = () =>
         originalRenderPage({
+          /* eslint-disable-next-line */
           enhanceApp: (App) => (props) =>
             sheet.collectStyles(<App {...props} />),
         });
