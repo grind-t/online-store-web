77628b0d39debb12251ddf9ee65b3dce4761cc4b
grind-t <grindt.gd@gmail.com>
2021-09-25 16:00:51 +0300

    move sample data to backend

--- app/firebase-admin/index.ts
+++ app/firebase-admin/index.ts
@@ -11,46 +11,4 @@ if (!admin.apps.length) {
   });
 }
 
-if (process.env.NODE_ENV === 'development') {
-  const bucket = admin.storage().bucket();
-  bucket
-    .upload('./public/images/product-placeholder.png', { public: true })
-    .then((response) => response[0].getMetadata())
-    .then((response) => response[0].mediaLink)
-    .then((image) => {
-      const product = {
-        name: 'Playstation Plus',
-        description: 'Описание товара',
-        image,
-        options: [
-          { name: 'Формат', values: ['Электронный'] },
-          {
-            name: 'Длительность',
-            values: ['1 месяц', '3 месяца', '12 месяцев'],
-          },
-        ],
-        variants: {
-          sample1: {
-            options: {
-              Формат: 'Электронный',
-              Длительность: '1 месяц',
-            },
-            price: 10000,
-            quantity: 1,
-          },
-          sample2: {
-            options: {
-              Формат: 'Электронный',
-              Длительность: '3 месяца',
-            },
-            price: 20000,
-            quantity: 1,
-          },
-        },
-      };
-      admin.firestore().collection('products').add(product);
-    })
-    .catch(console.error);
-}
-
 export default admin;
