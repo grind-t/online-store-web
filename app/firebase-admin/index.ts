01f77ff44e9b652aaeffa3683893d49261e5764c
grind-t <grindt.gd@gmail.com>
2021-11-22 22:25:05 +0300

    fix typescript error

--- app/firebase-admin/index.ts
+++ app/firebase-admin/index.ts
@@ -5,6 +5,7 @@ if (!admin.apps.length) {
     credential: admin.credential.cert({
       projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      // @ts-ignore
       privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     }),
     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
