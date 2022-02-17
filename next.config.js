e46b26ed62bbac2b3b86fbae5cd120334d849bb0
grind-t <grindt.gd@gmail.com>
2022-02-18 02:30:10 +0300

    build: replace babel plugin for styled components with swc replacement

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@ module.exports = {
   images: {
     domains: ['localhost', 'igomfvlbailflqrocfpv.supabase.in'],
   },
+  compiler: {
+    styledComponents: true,
+  },
   i18n: {
     locales: ['ru'],
     defaultLocale: 'ru',
