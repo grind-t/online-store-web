bff4b3f05345f756b4e34c91bddf7549d4d7c185
grind-t <grindt.gd@gmail.com>
2022-03-24 23:49:09 +0300

    refactor: custom input

--- components/common/utils/custom-input.tsx
+++ components/common/utils/custom-input.tsx
@@ -1,26 +1,24 @@
 import VisuallyHidden from 'components/common/utils/visually-hidden';
 import { InputHTMLAttributes } from 'react';
-import { useUID } from 'react-uid';
 import styled from 'styled-components';
+
+//#region styled
+const Label = styled.label`
+  display: inline-block;
+`;
+//#endregion
 
 const CustomInput = ({
   children,
   className,
   ...rest
 }: InputHTMLAttributes<HTMLInputElement>) => {
-  const uid = useUID();
   return (
-    <>
-      <VisuallyHidden as="input" id={uid} {...rest} />
-      <label htmlFor={uid} className={className}>
-        {children}
-      </label>
-    </>
+    <Label className={className}>
+      {children}
+      <VisuallyHidden as="input" {...rest} />
+    </Label>
   );
 };
 
-const Styled = styled(CustomInput)`
-  display: inline-block;
-`;
-
-export default Styled;
+export default CustomInput;
